7497c7bb7c2b67f712057347b597c4b011e2d907
Toru Ogasawara <ujina@me.com>
2022-07-26 12:55:51 +0900

    first commit

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,85 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'OGASHOSHI',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      
+      {
+        text: '業務案内',
+        link: '/guide/',
+      },
+      {
+        text: '報酬表',
+        link: '/fee/'
+      },
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'zizen-kakunin.md',
+          ]
+        }
+      ],
+      
+      '/fee/': [
+        {
+          title: 'Fee',
+          collapsable: false,
+        children: [
+          '',
+        ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ],
+
+  markdown: {
+    anchor: {
+      permalink: false
+    }
+  }
+
+}
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /ogashoshi_logo.jpeg  
+tagline: おがさわら行政書士事務所
+actionText: 事業復活支援金の事前確認について →
+actionLink: /guide/zizen-kakunin.html
+features:
+- title: 事業復活支援金の事前確認について
+  details: 面談・オンラインともに一律5000円（税込）にて承っております。
+- title: お気軽にご相談ください
+  details: 難しくないです、ゆっくり話します。
+- title: 他士業や専門業者を紹介します
+  details: お引き受けできないことでも詳しい方にお繋ぎします。
+footer: Made by Toru Ogasawara with ❤️
+---
